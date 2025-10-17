8efb4d83c78340e1bcdabf40fa75cc8fd056e40c
Priyanshi Mishra <priyanshimishra@itgeeks.com>
2025-10-17 19:59:50 +0530

    third commit

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,52 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+
+
+const slider = document.querySelector('.product-card-wrapper');
+let isDown = false;
+let startX;
+let scrollLeft;
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('dragging');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('dragging');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('dragging');
+});
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5;
+    slider.scrollLeft = scrollLeft - walk;
+});
+slider.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('touchmove', (e) => {
+    const x = e.touches[0].pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5;
+    slider.scrollLeft = scrollLeft - walk;
+});
+
+
+
+function scrollCards(direction) {
+    const wrapper = document.getElementById('cardWrapper');
+    const scrollAmount = 200;
+    if (direction === 'left') {
+        wrapper.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else {
+        wrapper.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    };
+};
